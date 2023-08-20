95afc41d2f9e6738f234d7bf8a5b1c96d6cef98e
Падий Николай Николаевич <padiynn@mosreg.ru>
2023-08-21 02:57:04 +0300

    pre-complited

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import os
-from datetime import datetime, timedelta
+import OpenSSL
+import datetime
+import requests
 
 
-LE_CERTS_PATH = '/etc/letsencrypt/live'
+LE_CERTS_PATH = '/etc/letsencrypt/live/'
+CERT = 'cert.pem'
+THRESHOLD = 7
 
 
-def list_domains():
+def list_domains() -> list:
     domains = []
     for file in os.listdir(LE_CERTS_PATH):
         fullname = os.path.join(LE_CERTS_PATH, file)
@@ -15,8 +19,32 @@ def list_domains():
     return domains
 
 
-def main():
-    print(list_domains())
+def cert_days_left(domain: str) -> int:
+    certfilepath = LE_CERTS_PATH + domain + '/' + CERT
+    with open(certfilepath, "r") as certfile:
+        cert_data = certfile.read()
+        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_data)
+    x509info = x509.get_notAfter()
+    cert_expires_dt = datetime.datetime.strptime(x509info.decode('ascii'), '%Y%m%d%H%M%SZ')
+    days_left = (cert_expires_dt - datetime.datetime.now()).days
+
+    return days_left
+
+
+def telegram_send_alert(domain: str, days_left: int) -> None:
+    TOKEN = 'token'
+    CHAT_ID = '-00000000'
+    message = f'ATTENTION! SSL for domain {domain} expire after {days_left} days'
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}'
+    requests.get(url)
+
+
+def main() -> None:
+    domains = list_domains()
+    for d in domains:
+        days_left = cert_days_left(d)
+        if days_left <= THRESHOLD:
+            telegram_send_alert(days_left)
 
 
 if __name__ == '__main__':
